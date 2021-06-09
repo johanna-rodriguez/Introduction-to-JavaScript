5e018c866872fa5f06311d7eb218673b10c36b03
Johanna Rodriguez <>
2021-06-08 23:03:16 -0500

    add task 7

--- index.js
+++ index.js
@@ -263,11 +263,21 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
-  /*Your Code here */
+function grade(score){
+    if(score >= 90){
+      return 'you got an A'
+    }else if(score >= 80){
+      return 'you got an B'
+    }else if(score >= 70){
+      return 'you got an C'
+    }else if(score >= 60){
+      return 'you got an D'
+    }else{
+      return 'you got an F'
+    }
   }
   
-  
+  console.log(grade(50))
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 
